7a680d503dcdc070af628b679f4eaf670063fb3a
sk <skomesarook@pm.me>
2023-07-03 12:50:51 -0400

    prog

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#define chief main
+#define yeet return
+#define sussin if
+#define bussin for
+#define tweakin while
+#define vibin std
+#define yikes endl
+#define ongod cout
+#define fr <<
+#define deadass >>
+#define rn ;
+#define mf int
+#define cap 0
+#define be =
+#define finna ==
+#define no !
+#define bouta ++
+
+#include <iostream>
+
+mf chief()
+{
+  mf t be cap rn
+      vibin::ongod fr "Enter a number larger than 1: " rn
+          vibin::cin deadass t rn
+          sussin(t < 2){
+              vibin::ongod fr "invalid input" fr vibin::yikes rn
+                  yeet no cap rn} bussin(mf i be 2; i <= t; bouta i)
+  {
+    tweakin(t % i finna cap)
+    {
+      vibin::ongod fr i fr vibin::yikes rn
+          t /= i rn
+    }
+  }
+  yeet cap rn
+}
